e09473e1ea6aff803c968798ef621b7e446d45d2
Rember Garcia <remberiori@gmail.com>
2020-02-08 01:46:47 -0600

    Mi primer commit

---
+++ app.js
@@ -0,0 +1,32 @@
+﻿var mes = "10"
+console.log(mes);
+  
+var mes = '1';
+switch (mes){
+    case "1":
+    case "2":
+    case "12":
+       alert("es invierno.") 
+       break;
+    case "3":
+    case "4":
+    case "5":
+       alert("es primavera.") 
+       break;      
+    case "6":
+    case "7":
+    case "8":
+           alert("es verano.") 
+        break;   
+    case "9":
+    case "10":
+    case "11":
+           alert("es otoño.") 
+        break;
+    case "13":
+        default:
+            alert("por favor selecciona un valor del 1 al 12."); 
+               
+}
+
+document.write("Rember Aurelio Quex Garcia Carné 20004461"); 
---
+++ imprimir.js
@@ -0,0 +1,4 @@
+var sign = prompt("Hola, cual es tu nombre?");
+var consola = sign;
+console.log(sign);
+document.write(sign);
